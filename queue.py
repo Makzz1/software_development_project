8037f506c4959d05c25b5f7f3143c26fa6f72661
maghizh <168180776+Makzz1@users.noreply.github.com>
2024-04-26 20:16:29 +0530

    Add files via upload

---
+++ queue.py
@@ -0,0 +1,23 @@
+class Queue:
+    def __init__(self):
+        self.arr = []
+        self.priority = 3
+        self.pos = 0
+
+    def add(self, ele):
+        if len(ele) > self.priority:
+        # elments are added to the array
+            self.arr.append(ele)
+        else:
+            self.arr.insert(self.pos,ele)
+            self.pos += 1
+
+    def remove(self):
+        # elements to be removed
+        if len(self.arr) > 0:
+            self.arr.pop(0)
+            self.pos -= 1
+        else:
+            print("there is not enough elements")
+
+
