4aaee11203b2ecf391d92d701f4febb40052c6e7
Makzz1 <maghizhvanban@gmail.com>
2024-05-03 14:06:48 +0530

    created connection between home and admin login

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print(8)
+import queue
+ele = {'move': 0, "order": {'briyani': 10, 'dosa': 5}}
+order_list= queue.Queue()
+order_list.add([1])
+order_list.add([1,2,3])
+order_list.add([1,2])
+order_list.add([1, 3, 4, 4, 4, 4, 1])
+order_count = ele['order']
+length = sum(order_count)
+print(length)
